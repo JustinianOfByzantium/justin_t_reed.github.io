c2b40a7061d193a8c8505ae267c4b3f06e1b7424
JustinianOfByzantium <justinian.reed@gmail.com>
2025-06-10 09:26:30 -0700

    added js to scroll to bottom of page

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("scrollButton");
+  const target = document.getElementById("targetSection");
+
+  button.addEventListener("click", function () {
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
